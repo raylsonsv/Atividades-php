88244bf8cc025cc02fd495430c4b124517518efd
raylsonsv <168122263+raylsonsv@users.noreply.github.com>
2024-07-25 14:58:50 -0300

    Add files via upload

---
+++ aula03.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+      echo "<h1>Contando de 1 a 10.</h1>";
+
+    for($x=1; $x<=10; $x++){
+        echo "$x <br>";
+    };
+
+
+    echo "<h1>Tabela de 1 a 10 em PHP</h1>";
+    
+    ?>
+
+
+</body>
+</html>
